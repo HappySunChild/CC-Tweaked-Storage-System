c063db8fa9c9717503bc6b37efc43508b44671f7
HappySunChild <nothappysunchild@gmail.com>
2025-01-11 16:06:07 -0500

    small changes

--- storagesystem/utility/numbers.lua
+++ storagesystem/utility/numbers.lua
@@ -1,17 +1,17 @@
-local mathUtility = {}
+local numbers = {}
 
 local abbreviations = {'K', 'M', 'B', 'T', 'Qa', 'Qi'}
 
 ---@param number number
 ---@return string
-function mathUtility.abbreviate(number)
+function numbers.abbreviate(number)
 	if number < 1000 then
 		return tostring(number)
 	end
 	
-	local index = math.floor(math.log(number + 1, 10) / 3)
+	local index = math.floor(math.log10(number + 1) / 3)
 	
 	return string.format('%.1f%s', number / 10 ^ (index * 3), abbreviations[index])
 end
 
-return mathUtility
+return numbers
